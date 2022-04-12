07de9b440b79fe319d69c24f471ddfad54cc5f7c
Daniel Blagy <daniel.blagy@gmail.com>
2022-04-12 14:52:46 +0300

    removed obsolete code

--- service/users-service.go
+++ service/users-service.go
@@ -15,51 +15,12 @@ type UsersService interface {
 
 type UsersServiceProvider struct {
 	database *gorm.DB
-	users    []entity.User
 }
 
 func CreateUsersService(database *gorm.DB) UsersService {
 	return &UsersServiceProvider{
 		database: database,
-		users:    []entity.User{},
-		/*users: []entity.User{
-			{Id: 1, Login: "userOne", FullName: "Dave", Password: "12345", Articles: []entity.Article{}},
-			{Id: 2, Login: "user22", FullName: "Jane", Password: "8888", Articles: []entity.Article{}},
-			{Id: 3, Login: "userTres", FullName: "Maggie", Password: "37212asd", Articles: []entity.Article{}},
-		},*/
 	}
-}
-
-func (service *UsersServiceProvider) findUser(idStr string) (entity.User, error) {
-	/*id, err := strconv.Atoi(idStr)
-	if err != nil {
-		return entity.User{}, errors.New("server error")
-	}
-
-	for _, user := range service.users {
-		if user.Id == id {
-			return user, nil
-		}
-	}
-
-	return entity.User{}, errors.New("user not found")*/
-	return entity.User{}, nil
-}
-
-func (service *UsersServiceProvider) findUserIndex(idStr string) (int, error) {
-	/*id, err := strconv.Atoi(idStr)
-	if err != nil {
-		return -1, errors.New("server error")
-	}
-
-	for i, user := range service.users {
-		if user.Id == id {
-			return i, nil
-		}
-	}
-
-	return -1, errors.New("user not found")*/
-	return 0, nil
 }
 
 func (service *UsersServiceProvider) GetAll() ([]entity.User, error) {
@@ -80,14 +41,6 @@ func (service *UsersServiceProvider) Create(user entity.User) (entity.User, erro
 }
 
 func (service *UsersServiceProvider) Update(id string, user entity.User) (entity.User, error) {
-	/*i, err := service.findUserIndex(id)
-
-	if err == nil {
-		service.users[i] = user
-	}
-
-	return user, err*/
-
 	result := service.database.Save(&user)
 	return user, result.Error
 }
