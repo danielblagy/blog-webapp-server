c63536caadf804bab4b499fc0e966cdfa451788c
Daniel Blagy <daniel.blagy@gmail.com>
2022-04-12 21:14:55 +0300

    properly hiding sensitive info (i.e. password hash) in server responses

--- entity/user.go
+++ entity/user.go
@@ -1,9 +1,19 @@
 package entity
 
+import "encoding/json"
+
 type User struct {
 	Id       int       `json:"id" gorm:"primaryKey"`
 	Login    string    `json:"login" gorm:"type:varchar(100);uniqueIndex;not null"`
 	FullName string    `json:"fullname" gorm:"type:varchar(300);not null"`
-	Password string    `json:"password" gorm:"type:text;not null"`
+	Password string    `json:"password,omitempty" gorm:"type:text;not null"`
 	Articles []Article `json:"articles" gorm:"foreignKey:AuthorId"`
+}
+
+// remove sensitive imformation from user data in server responses
+func (u User) MarshalJSON() ([]byte, error) {
+	type user User // prevent recursion
+	x := user(u)
+	x.Password = "" // we set omitempty in User type, and here we make password empty, so the password propery will be ommited
+	return json.Marshal(x)
 }
