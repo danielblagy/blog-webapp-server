52623dc1d488790ae94f3c980cf0e387bf55ae64
Daniel Blagy <daniel.blagy@gmail.com>
2022-04-12 15:06:47 +0300

    improved gorm models

--- entity/article.go
+++ entity/article.go
@@ -1,9 +1,9 @@
 package entity
 
 type Article struct {
-	Id        int `json:"id" gorm:"primaryKey"`
-	AuthorId  int
-	Title     string
-	Content   string
-	Published bool
+	Id        int    `json:"id" gorm:"primaryKey"`
+	AuthorId  int    `json:"author_id" gorm:"not null"`
+	Title     string `json:"title" gorm:"type:varchar(300);not null"`
+	Content   string `json:"content" gorm:"type:text;not null"`
+	Published bool   `json:"published" gorm:"not null"`
 }
--- entity/user.go
+++ entity/user.go
@@ -2,8 +2,8 @@ package entity
 
 type User struct {
 	Id       int       `json:"id" gorm:"primaryKey"`
-	Login    string    `json:"login" gorm:"type varchar(100);uniqueIndex;not null"`
-	FullName string    `json:"fullname" gorm:"not null"`
-	Password string    `json:"password" gorm:"not null"`
+	Login    string    `json:"login" gorm:"type:varchar(100);uniqueIndex;not null"`
+	FullName string    `json:"fullname" gorm:"type:varchar(300);not null"`
+	Password string    `json:"password" gorm:"type:varchar(32);not null"`
 	Articles []Article `json:"articles" gorm:"foreignKey:AuthorId"`
 }
