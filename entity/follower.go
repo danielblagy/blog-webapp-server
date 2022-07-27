c5e1c5fce18c977b350e1888d9daf79635f3bcdd
Daniel Blagy <daniel.blagy@gmail.com>
2022-07-27 19:06:07 +0300

    /users/follow won't add another record into the table, because the follower-follows pairs are now unique

--- entity/follower.go
+++ entity/follower.go
@@ -1,6 +1,6 @@
 package entity
 
 type Follower struct {
-	FollowerId int `json:"follower_id" gorm:"not null;index"`
-	FollowsId  int `json:"follows_id" gorm:"not null;index"`
+	FollowerId int `json:"follower_id" gorm:"not null;uniqueIndex:idx_follower_follows"`
+	FollowsId  int `json:"follows_id" gorm:"not null;uniqueIndex:idx_follower_follows"`
 }
